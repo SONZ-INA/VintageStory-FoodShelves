2229c67f991d893b6b1cc884f313eda95e6ee0c4
Hans <shrinerh@gmail.com>
2026-07-03 19:30:24 -0400

    3.0.6 - Add label with liquid model to the front of racked barrels with liquid in them

--- code/BlockEntity/Barrels/BEBarrelRack.cs
+++ code/BlockEntity/Barrels/BEBarrelRack.cs
@@ -99,8 +99,18 @@ public class BEBarrelRack : BEBaseFSContainer {
 
         ItemStack[] stack = GetContentStacks();
         if (stack[0]?.Block != null) {
-            MeshData? substituteBarrelShape = SubstituteBlockShape(Api, tesselator, ShapeReferences.HorizontalBarrel, stack[0].Block);
-            currentMesh?.AddMeshData(substituteBarrelShape?.BlockYRotation(block));
+            MeshData? barrelMesh = SubstituteBlockShape(Api, tesselator, ShapeReferences.HorizontalBarrel, stack[0].Block);
+            currentMesh?.AddMeshData(barrelMesh?.BlockYRotation(block));
+
+            if (!inv[1].Empty) {
+                try {
+                    MeshData? labelMesh = GenBarrelLabelMesh(Api as ICoreClientAPI, inv[1], ShapeReferences.utilBarrelLabel);
+                    currentMesh?.AddMeshData(labelMesh?.BlockYRotation(block));
+                }
+                catch (Exception e) {
+                    Api?.Logger.Warning("[FoodShelves] BarrelRack label mesh failed: " + e.Message);
+                }
+            }
         }
 
         mesher.AddMeshData(currentMesh);
